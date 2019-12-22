3461562be89f1c0776acb4c2caf5cb5951f10df0
brye <bryewalks@gmail.com>
2019-12-22 00:50:19 -0600

    added user authorization using my gem called simple_jwt_auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_052847) do
+ActiveRecord::Schema.define(version: 2019_12_22_064326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2019_12_19_052847) do
     t.string "publisher"
     t.string "image_url"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "simple_jwt_auth_users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
